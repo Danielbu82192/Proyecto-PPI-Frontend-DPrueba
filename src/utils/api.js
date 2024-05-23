11e132e42a6a30d9b0cad63e447eeae44f615d18
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-05-23 13:17:15 -0500

    Prueba Nueva

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
-
+ 
 module.exports = async (req, res) => {
   if (req.method !== "GET") {
     res.status(405).json({ message: "Method not allowed" });

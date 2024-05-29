8ead4b7757f4b33f96621d18084d9c0307abe0d1
Danielbu82192 <62263395+Danielbu82192@users.noreply.github.com>
2024-05-29 11:55:00 -0500

    validacion rol

--- src/app/component/coordinador/crearAsesor/page.jsx
+++ src/app/component/coordinador/crearAsesor/page.jsx
@@ -1,32 +1,46 @@
 "use client";
 import React, { useEffect, useState } from 'react';
 import './page.css';
+import CryptoJS from 'crypto-js';
+import { useRouter } from "next/navigation";
 //import Dialog from '@mui/material/Dialog';
 
 const Page = () => {
-  const [asesores, setAsesores] = useState(Array(15).fill({ Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "",Horas_Asesor:"",Oficina:"" }));
+  const [asesores, setAsesores] = useState(Array(15).fill({ Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "", Horas_Asesor: "", Oficina: "" }));
   const [traidosDeBD, setTraidosDeBD] = useState([]); // Almacenar los datos originales
   const [horas, setHoras] = useState([]); // Almacenar los datos originales
   const apiEndpoint = "https://td-g-production.up.railway.app/usuario/GetAsesor";
   const getAllUsers = "https://td-g-production.up.railway.app/usuario";
 
+  const router = useRouter();
+
+
   useEffect(() => {
     fetch(apiEndpoint)
       .then(response => response.json())
-      .then(data => { 
+      .then(data => {
         const fetchedData = data.slice(0, 15);
         const filledData = fetchedData.concat(Array(15 - fetchedData.length).fill({ Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "" }));
         setAsesores(filledData);
         // Almacenar los datos originales
         setTraidosDeBD(fetchedData);
       })
-      .catch(error => console.error('Error fetching data:', error)); 
-       
+      .catch(error => console.error('Error fetching data:', error));
+    const validarRol = () => {
+      const usuarioNest = localStorage.getItem('U2FsdGVkX1');
+      const bytes = CryptoJS.AES.decrypt(usuarioNest, 'PPIITYTPIJC');
+      const usuarioN = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+      if (usuarioN.rol.id != 4) {
+        router.push('/')
+      }
+
+    }
+    validarRol()
   }, []);
 
-  const handleDelete = (index, asesores) => { 
+  const handleDelete = (index, asesores) => {
     const updatedAsesores = [...asesores];
-    updatedAsesores[index] = { Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "", Horas_Asesor: "", Oficina: ""  };
+    updatedAsesores[index] = { Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "", Horas_Asesor: "", Oficina: "" };
     setAsesores(updatedAsesores);
   };
 
@@ -36,9 +50,9 @@ const Page = () => {
 
     /* Si asesor.Usuario_Documento === null || asesor.Usuario_Nombre === null || asesor.Usuario_Correo === null, volver Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "" */
     if (updatedAsesores[index].Usuario_Documento === null && updatedAsesores[index].Usuario_Nombre === null && updatedAsesores[index].Usuario_Correo === null) {
-      updatedAsesores[index] = { Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "" , Horas_Asesor: "", Oficina: ""  };
+      updatedAsesores[index] = { Usuario_ID: "", Usuario_Nombre: "", Rol_ID: "", Usuario_Documento: "", Usuario_Correo: "", Horas_Asesor: "", Oficina: "" };
     }
-    
+
     setAsesores(updatedAsesores);
   };
 
@@ -47,7 +61,7 @@ const Page = () => {
     let asesoresModificados = asesores.filter((asesor) => {
       if (asesor.Usuario_ID !== "") {
         const asesorOriginal = traidosDeBD.find((asesorOriginal) => asesorOriginal.Usuario_ID === asesor.Usuario_ID);
-        return asesor.Usuario_Documento !== asesorOriginal.Usuario_Documento || asesor.Usuario_Nombre !== asesorOriginal.Usuario_Nombre || asesor.Usuario_Correo !== asesorOriginal.Usuario_Correo|| asesor.Horas_Asesor !== asesorOriginal.Oficina;
+        return asesor.Usuario_Documento !== asesorOriginal.Usuario_Documento || asesor.Usuario_Nombre !== asesorOriginal.Usuario_Nombre || asesor.Usuario_Correo !== asesorOriginal.Usuario_Correo || asesor.Horas_Asesor !== asesorOriginal.Oficina;
       }
       return true;
     });
@@ -96,13 +110,13 @@ const Page = () => {
       asesoresNuevos,
       asesoresEliminados
     };
- 
+
     let dataToSend2 = asesoresModificados.concat(asesoresNuevos, asesoresEliminados);
     console.log(dataToSend2)
     //console.log("antes de filtrar: ", dataToSend2);  
     let conjunto = new Set(dataToSend2.map(JSON.stringify));
     dataToSend2 = Array.from(conjunto).map(JSON.parse);
-    
+
     //  console.log("Filtrado:", dataToSend2);
 
     try {
@@ -125,75 +139,85 @@ const Page = () => {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Asesores PPI</h1>
-      <table className="min-w-full bg-white border border-gray-200">
-        <thead>
-          <tr>
-            <th className="py-1 px-4 bg-transparent border-gray-200"></th>
-            <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Cédula</th>
-            <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Nombre</th>
-            <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Correo</th>
-            <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Horas Asesorías</th>
-            <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Oficina</th>
-          </tr>
-        </thead>
-        <tbody>
-          {asesores.map((asesor, index) => (
-            <tr key={index} className="hover:bg-gray-100">
-              <td className="py-1 px-0.5 border-gray-200 text-center text-sm">
-                 {(asesor.Usuario_ID !== ("" && null) && asesor.Usuario_Documento !== ("" && null) || asesor.Usuario_Nombre !== ("" && null) || asesor.Usuario_Correo !== ("" && null)) ? (
-                  <button onClick={() => handleDelete(index, asesores)} className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-700 text-sm">X</button>
-                )
-                  : null
-                }
-              </td>
-              <td className="p-0 border-gray-200">
-                <input
-                  type="text"
-                  value={asesor.Usuario_Documento || ''}
-                  onChange={(e) => handleInputChange(index, 'Usuario_Documento', e.target.value)}
-                  className="w-full py-1 px-2 border-gray-200"
-                />
-              </td>
-              <td className="p-0 border-gray-200" style={{ width: "35%" }}>
-                <input
-                  type="text"
-                  value={asesor.Usuario_Nombre || ''}
-                  onChange={(e) => handleInputChange(index, 'Usuario_Nombre', e.target.value)}
-                  className="w-full py-1 px-2 border-gray-200"
-                />
-              </td>
-              <td className="p-0 border-gray-200" style={{ width: "45%" }}>
-                <input
-                  type="email"
-                  value={asesor.Usuario_Correo || ''}
-                  onChange={(e) => handleInputChange(index, 'Usuario_Correo', e.target.value)}
-                  className="w-full py-1 px-2 border-gray-200"
-                />
-              </td>
-              <td className="p-0 border-gray-200" style={{ width: "45%" }}>
-                <input
-                  type="email"
-                  value={asesor.Horas_Asesor }
-                  onChange={(e) => handleInputChange(index, 'Horas_Asesor', e.target.value)}
-                  className="w-full py-1 px-2 border-gray-200"
-                />
-              </td>
-              <td className="p-0 border-gray-200" style={{ width: "45%" }}>
-              <input
-                type="email"
-                value={asesor.Oficina }
-                onChange={(e) => handleInputChange(index, 'Oficina', e.target.value)}
-                className="w-full py-1 px-2 border-gray-200"
-              />
-            </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={handleSave} className="mt-2 px-4 py-1 bg-green-500 text-white rounded hover:bg-green-700">Guardar Cambios</button>
+
+
+    <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
+      <div className='pt-8  pb-8 w-full'>
+        <div className='w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
+          <h1 className='text-4xl font-bold text-gray-600'>Asesores PPI</h1>
+        </div>
+        <div className='p-10'>
+          <table className="min-w-full bg-white border border-gray-200">
+            <thead>
+              <tr>
+                <th className="py-1 px-4 bg-transparent border-gray-200"></th>
+                <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Cédula</th>
+                <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Nombre</th>
+                <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Correo</th>
+                <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Horas Asesorías</th>
+                <th className="py-1 px-2 border-b border-gray-200 bg-green-500">Oficina</th>
+              </tr>
+            </thead>
+            <tbody>
+              {asesores.map((asesor, index) => (
+                <tr key={index} className="hover:bg-gray-100">
+                  <td className="py-1 px-0.5 border-gray-200 text-center text-sm">
+                    {(asesor.Usuario_ID !== ("" && null) && asesor.Usuario_Documento !== ("" && null) || asesor.Usuario_Nombre !== ("" && null) || asesor.Usuario_Correo !== ("" && null)) ? (
+                      <button onClick={() => handleDelete(index, asesores)} className="px-2 py-1 bg-red-500 text-white rounded hover:bg-red-700 text-sm">X</button>
+                    )
+                      : null
+                    }
+                  </td>
+                  <td className="p-0 border-gray-200">
+                    <input
+                      type="text"
+                      value={asesor.Usuario_Documento || ''}
+                      onChange={(e) => handleInputChange(index, 'Usuario_Documento', e.target.value)}
+                      className="w-full py-1 px-2 border-gray-200"
+                    />
+                  </td>
+                  <td className="p-0 border-gray-200" style={{ width: "35%" }}>
+                    <input
+                      type="text"
+                      value={asesor.Usuario_Nombre || ''}
+                      onChange={(e) => handleInputChange(index, 'Usuario_Nombre', e.target.value)}
+                      className="w-full py-1 px-2 border-gray-200"
+                    />
+                  </td>
+                  <td className="p-0 border-gray-200" style={{ width: "45%" }}>
+                    <input
+                      type="email"
+                      value={asesor.Usuario_Correo || ''}
+                      onChange={(e) => handleInputChange(index, 'Usuario_Correo', e.target.value)}
+                      className="w-full py-1 px-2 border-gray-200"
+                    />
+                  </td>
+                  <td className="p-0 border-gray-200" style={{ width: "45%" }}>
+                    <input
+                      type="email"
+                      value={asesor.Horas_Asesor}
+                      onChange={(e) => handleInputChange(index, 'Horas_Asesor', e.target.value)}
+                      className="w-full py-1 px-2 border-gray-200"
+                    />
+                  </td>
+                  <td className="p-0 border-gray-200" style={{ width: "45%" }}>
+                    <input
+                      type="email"
+                      value={asesor.Oficina}
+                      onChange={(e) => handleInputChange(index, 'Oficina', e.target.value)}
+                      className="w-full py-1 px-2 border-gray-200"
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button onClick={handleSave} className="mt-2 px-4 py-1 bg-green-500 text-white rounded hover:bg-green-700">Guardar Cambios</button>
+
+        </div>
+      </div>
     </div>
+
   );
 };
 

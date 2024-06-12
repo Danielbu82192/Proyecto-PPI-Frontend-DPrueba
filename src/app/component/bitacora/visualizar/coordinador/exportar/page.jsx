9540bdc5f5fa15af4a649d7acb4905aa4c333518
Keny Alejandro Serrato Ávila <kserratoa@gmail.com>
2024-06-12 15:02:39 -0500

    Update Fix

--- src/app/component/bitacora/visualizar/coordinador/exportar/page.jsx
+++ src/app/component/bitacora/visualizar/coordinador/exportar/page.jsx
@@ -1,26 +1,14 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React, { useState, useEffect } from 'react'
-import { useRouter } from "next/navigation";
+import React, { useState } from 'react';
+import * as XLSX from 'xlsx';
 
-function page() {
-
-    const [showAlertDelete, setShowAlertDelete] = useState(false);
-    const [showCorrecto, setShowCorrecto] = useState(false);
-    const router = useRouter();
-    const [downloading, setDownloading] = useState(false);
-
-    const currentYear = new Date().getFullYear();
-    const currentMonth = new Date().getMonth() + 1;
-
-    let semester;
-    if (currentMonth >= 1 && currentMonth <= 6) {
-        semester = 1;
-    } else {
-        semester = 2;
-    }
-
-
+function Page() {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [showMessage, setShowMessage] = useState(false);
+    const [showSecondMessage, setShowSecondMessage] = useState(false);
+    const [showTMessage, setShowTMessage] = useState(false);
 
     const fetchDataAndExportExcel = async () => {
         setLoading(true);
@@ -118,134 +106,54 @@ function page() {
         XLSX.writeFile(workbook, filename);
     };
 
-    const fileName = `Backup PPI ${currentYear} - ${semester}.zip`;
-
-    const Limpiar = async () => {
-        showAlertDelete(false)
-        fetchDataAndExportNewExcel()
-        fetchDataAndExportAnotherExcel()
-        fetchDataAndExportExcel()
-        const response = await fetch('https://td-g-production.up.railway.app/equipo-ppi/exportar/-1');
-        const filePath = await response.text()
-        const baseUrl = 'https://td-g-production.up.railway.app';
-        const fileUrl = new URL(filePath.replace('/public', ''), baseUrl).href;
-        const newWindow = window.open(fileUrl, '_blank');
-        if (newWindow) {
-            setTimeout(() => {
-                newWindow.close();
-            }, 2000);
-            const response = await fetch('https://td-g-production.up.railway.app/hora-semanal/exportar/');
-            const filePath = await response.text()
-            const baseUrl = 'https://td-g-production.up.railway.app';
-            const fileUrl = new URL(filePath.replace('/public', ''), baseUrl).href;
-            const newWindow = window.open(fileUrl, '_blank');
-            if (newWindow) {
-                setTimeout(() => {
-                    newWindow.close();
-                }, 2000);
-                try {
-                    setDownloading(true);
-                    const response = await axios.get(
-                        "https://td-g-production.up.railway.app/backup/download-and-clean-bucket",
-                        {
-                            responseType: "blob",
-                        }
-                    );
-
-                    const url = window.URL.createObjectURL(new Blob([response.data]));
-                    const link = document.createElement("a");
-                    link.href = url;
-                    link.setAttribute("download", fileName);
-                    document.body.appendChild(link);
-                    link.click();
-                    link.parentNode.removeChild(link);
-                    setDownloading(false);
-                } catch (error) {
-                    console.error("Error:", error);
-                    setDownloading(false);
-                }
-            }
-
-        }
-    }
-
-
-    useEffect(() => {
-        if (showCorrecto) {
-            const timer = setTimeout(() => {
-                setShowCorrecto(false);
-            }, 2000);
-
-            return () => clearTimeout(timer);
-        }
-    }, [showCorrecto]);
     return (
-        <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
-            <div className='pt-8  pb-8 w-full'>
-                <div className='  h-22 pb-2 flex-col  border-b-2 flex justify-between items-center'>
-                    <div>
-                        <h1 className='ml-5 text-4xl font-bold text-gray-600'>Limpiar sistema</h1>
-                    </div>
+        <div className="ml-2 mr-6 mt-6 border bg-white border-b rounded-lg shadow-lg">
+            <div className='pt-8 pb-8 w-full text-center'> 
+                <div className='pt-5 w-full border-b-2 flex items-center sm:items-start justify-center sm:justify-start sm:pl-8 sm:h-22 pb-5 text-center sm:text-left'>
+                    <h1 className='text-4xl font-bold text-gray-600'>Generación de Reportes</h1>
                 </div>
-                <div className='p-10'>
-                    <div className='text-xl text-gray-500'>
-                        <span>Para poder eliminar información del sistema, se debe tener en cuenta que, cuando se realice esta acción, se eliminará la siguiente información:</span>
-                        <ul class="list-disc ml-10">
-                            <li class="py-2">Citas de asesorías</li>
-                            <li class="py-2">Bitácoras</li>
-                            <li class="py-2">Semanas</li>
-                            <li class="py-2">Notificaciones</li>
-                        </ul>
-                        <button onClick={() => { setShowAlertDelete(true); }} className="mt-6 text-white py-2 px-4 w-full rounded bg-red-400 hover:bg-red-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5 flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-white">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                            </svg>
-                            Limpar sistema
+                <br />
+                <div className="p-4">
+                    <p className="text-justify">Selecciona el reporte que deseas generar:</p>
+                </div>
+                <div className='p-4 flex flex-col items-center'>
+                    <div className="flex space-x-4">
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={fetchDataAndExportExcel}
+                            onMouseEnter={() => setShowMessage(true)}
+                            onMouseLeave={() => setShowMessage(false)}
+                        >
+                            Equipos con Mora
                         </button>
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={fetchDataAndExportAnotherExcel}
+                            onMouseEnter={() => setShowSecondMessage(true)}
+                            onMouseLeave={() => setShowSecondMessage(false)}
+                        >
+                            Docentes con Mora
+                        </button>
+                        <button
+                            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                            onClick={fetchDataAndExportNewExcel}
+                            onMouseEnter={() => setShowTMessage(true)}
+                            onMouseLeave={() => setShowTMessage(false)}
+                        >
+                            Estudiantes sin Asesoría
+                        </button> 
                     </div>
+                    {showMessage && <p className="text-sm text-gray-500 mt-2">Genera un reporte que especifica cuáles entregas no han sido cargadas por los estudiantes.</p>}
+                    {showSecondMessage && <p className="text-sm text-gray-500 mt-2">Genera un reporte que especifica cuáles entregas no han sido calificadas por los docentes.</p>}
+                    {showTMessage && <p className="text-sm text-gray-500 mt-2">Genera un reporte que especifica cuáles estudiantes no han recibido su nota total de asesorías.</p>}
+                </div>
+                <div className='p-4'>
+                    {loading && <p>Cargando...</p>}
+                    {error && <p>Error: {error}</p>}
                 </div>
             </div>
-            {showAlertDelete && (<>
-                <div className="fixed inset-0 z-10 bg-grey bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
-                    <div class="flex flex-col justify-center content-center items-center rounded-lg bg-white p-4 shadow-2xl min-w-[50vw] max-w-[50vw] border border-solid border-gray-300">
-                        <h2 class="text-lg font-bold">¿Deseas limpiar el sistema?</h2>
-                        <p class="mt-2 text-sm text-gray-800">
-                            Está seguro de que desea limpiar el sistema, al limpiar el sistema se exportaran la información.
-                        </p>
-
-                        <div class="mt-4 flex flex-row flex-wrap min-w-full items-center content-center justify-center gap-2">
-                            <button type="button" class="min-w-[25%] rounded-lg bg-green-400 px-4 py-2 text-sm font-medium text-white hover:bg-green-500" onClick={() => Limpiar()}>
-                                Confirmar
-                            </button>
-                            <button type="button" class="min-w-[25%] rounded-lg bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500" onClick={() => setShowAlertDelete(false)}>
-                                Cancelar
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </>
-            )}
-            {
-                showCorrecto && (
-                    <div className="fixed bottom-0 right-0 mb-8 mr-8">
-                        <div className="flex w-96 shadow-lg rounded-lg">
-                            <div class="bg-green-600 py-4 px-6 rounded-l-lg flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
-                            </div>
-                            <div className="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
-                                <div>El sistema se limpió correctamente.</div>
-                                <button onClick={() => { setShowCorrecto(!showCorrecto) }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current text-gray-700" viewBox="0 0 16 16" width="20" height="20">
-                                        <path fillRule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                )
-            }
-        </div >
-    )
+        </div>
+    );
 }
 
-export default page
+export default Page;
--- src/app/component/coordinador/limpiarSistema/page.jsx
+++ src/app/component/coordinador/limpiarSistema/page.jsx
@@ -1,52 +1,79 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 "use client"
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
+import axios from 'axios';
+import * as XLSX from 'xlsx';
 
-function page() {
-
+function Page() {
     const [showAlertDelete, setShowAlertDelete] = useState(false);
     const [showCorrecto, setShowCorrecto] = useState(false);
+    const [downloading, setDownloading] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
     const router = useRouter();
 
-    const Limpiar = async () => {
-        showAlertDelete(false) 
-        const response = await fetch('https://td-g-production.up.railway.app/equipo-ppi/exportar/-1' );
-        const filePath = await response.text()
-        const baseUrl = 'https://td-g-production.up.railway.app';
-        const fileUrl = new URL(filePath.replace('/public', ''), baseUrl).href;
-        const newWindow = window.open(fileUrl, '_blank'); 
-        if (newWindow) {
-            setTimeout(() => {
-                newWindow.close();
-            }, 2000);
-            const response = await fetch('https://td-g-production.up.railway.app/hora-semanal/exportar/');
-            const filePath = await response.text()
-            const baseUrl = 'https://td-g-production.up.railway.app';
-            const fileUrl = new URL(filePath.replace('/public', ''), baseUrl).href;
-            const newWindow = window.open(fileUrl, '_blank');
-            if (newWindow) {
-                setTimeout(() => {
-                    newWindow.close();
-                }, 2000);
-                
-                setShowCorrecto(true) 
-                const requestOptions = {
-                    method: 'DELETE',
-                    headers: { 'Content-Type': 'application/json' },
-                };
-                const response = await fetch('https://td-g-production.up.railway.app/usuario/limpiarSistema/', requestOptions);
-                if (response.ok) {
-                    setShowCorrecto(true)
-                    setTimeout(() => {
-                        router.push('/');
-                    }, 2000);
-                }
-            }
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth() + 1;
+    const semester = currentMonth >= 1 && currentMonth <= 6 ? 1 : 2;
+    const fileName = `Backup PPI ${currentYear} - ${semester}.zip`;
 
+    const exportToExcel = (data, prefix) => {
+        const worksheet = XLSX.utils.json_to_sheet(data);
+        const workbook = XLSX.utils.book_new();
+        XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
 
+        const currentDate = new Date().toLocaleString("es-CO", { timeZone: "America/Bogota" });
+        const [date, time] = currentDate.split(', ');
+        const formattedDate = date.replace(/\//g, '-');
+        const formattedTime = time.replace(/:/g, '-').replace(' ', '');
+        const filename = `${prefix} | ${formattedDate} ${formattedTime}.xlsx`;
+
+        XLSX.writeFile(workbook, filename);
+    };
+
+    const fetchDataAndExportExcel = async (url, prefix) => {
+        setLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(url);
+            if (!response.ok) throw new Error('Network response was not ok');
+            const result = await response.json();
+            setData(result);
+            exportToExcel(result, prefix);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
         }
-    }
+    };
+
+    const Limpiar = async () => {
+        setShowAlertDelete(false);
+
+        try {
+            setDownloading(true);
+            const response = await axios.get(
+                "https://td-g-production.up.railway.app/backup/download-and-clean-bucket",
+                {
+                    responseType: "blob",
+                }
+            );
+
+            const url = window.URL.createObjectURL(new Blob([response.data]));
+            const link = document.createElement("a");
+            link.href = url;
+            link.setAttribute("download", fileName);
+            document.body.appendChild(link);
+            link.click();
+            link.parentNode.removeChild(link);
+            setDownloading(false);
+        } catch (error) {
+            console.error("Error:", error);
+            setDownloading(false);
+        }
+    };
 
     useEffect(() => {
         if (showCorrecto) {
@@ -57,73 +84,60 @@ function page() {
             return () => clearTimeout(timer);
         }
     }, [showCorrecto]);
+
     return (
-        <div className="ml-6 mr-6 mt-6 border   bg-white border-b flex justify-between">
-            <div className='pt-8  pb-8 w-full'>
-                <div className='  h-22 pb-2 flex-col  border-b-2 flex justify-between items-center'>
+        <div className="ml-6 mr-6 mt-6 border bg-white border-b flex justify-between">
+            <div className='pt-8 pb-8 w-full'>
+                <div className='h-22 pb-2 flex-col border-b-2 flex justify-between items-center'>
                     <div>
-                        <h1 className='ml-5 text-4xl font-bold text-gray-600'>Limpiar sistema</h1>
+                        <h1 className='ml-5 text-4xl font-bold text-gray-600'>Limpiar Sistema</h1>
                     </div>
                 </div>
                 <div className='p-10'>
                     <div className='text-xl text-gray-500'>
                         <span>Para poder eliminar información del sistema, se debe tener en cuenta que, cuando se realice esta acción, se eliminará la siguiente información:</span>
-                        <ul class="list-disc ml-10">
-                            <li class="py-2">Citas de asesorías</li>
-                            <li class="py-2">Bitácoras</li>
-                            <li class="py-2">Semanas</li>
-                            <li class="py-2">Notificaciones</li>
+                        <ul className="list-disc ml-10">
+                            <li className="py-2">Citas de asesorías</li>
+                            <li className="py-2">Bitácoras</li>
+                            <li className="py-2">Semanas</li>
+                            <li className="py-2">Notificaciones</li>
                         </ul>
-                        <button onClick={() => { setShowAlertDelete(true); }} className="mt-6 text-white py-2 px-4 w-full rounded bg-red-400 hover:bg-red-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5 flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8 text-white">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        <button onClick={() => setShowAlertDelete(true)} className="mt-6 text-white py-2 px-4 w-full rounded bg-red-400 hover:bg-red-500 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5 flex items-center justify-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9L14.394 18H9.606L9.26 9M18.228 5.79C18.57 5.842 18.91 5.897 19.25 5.956M18.228 5.79L16.16 19.673A2.25 2.25 0 0113.916 21.75H8.084A2.25 2.25 0 015.84 19.673L3.772 5.79M18.228 5.79A48.108 48.108 0 0014.75 5.393M3.772 5.79C3.432 5.731 3.092 5.676 2.75 5.617M3.772 5.79A48.11 48.11 0 017.25 5.393M9.75 5.393V4.477C9.75 3.297 8.84 2.313 7.66 2.276A51.964 51.964 0 004.34 2.276C3.16 2.313 2.25 3.297 2.25 4.477V5.393M9.75 5.393A48.667 48.667 0 007.25 5.393" />
                             </svg>
-                            Limpar sistema
+                            Limpiar Sistema
                         </button>
                     </div>
                 </div>
             </div>
-            {showAlertDelete && (<>
+            {showAlertDelete && (
                 <div className="fixed inset-0 z-10 bg-grey bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
-                    <div class="flex flex-col justify-center content-center items-center rounded-lg bg-white p-4 shadow-2xl min-w-[50vw] max-w-[50vw] border border-solid border-gray-300">
-                        <h2 class="text-lg font-bold">¿Deseas limpiar el sistema?</h2>
-                        <p class="mt-2 text-sm text-gray-800">
+                    <div className="flex flex-col justify-center content-center items-center rounded-lg bg-white p-4 shadow-2xl min-w-[50vw] max-w-[50vw] border border-solid border-gray-300">
+                        <h2 className="text-lg font-bold">¿Deseas limpiar el sistema?</h2>
+                        <p className="mt-2 text-sm text-gray-800">
                             Está seguro de que desea limpiar el sistema, al limpiar el sistema se exportaran la información.
                         </p>
-
-                        <div class="mt-4 flex flex-row flex-wrap min-w-full items-center content-center justify-center gap-2">
-                            <button type="button" class="min-w-[25%] rounded-lg bg-green-400 px-4 py-2 text-sm font-medium text-white hover:bg-green-500" onClick={() => Limpiar()}>
+                        <div className="mt-4 flex flex-row flex-wrap min-w-full items-center content-center justify-center gap-2">
+                            <button type="button" className="min-w-[25%] rounded-lg bg-green-400 px-4 py-2 text-sm font-medium text-white hover:bg-green-500" onClick={Limpiar}>
                                 Confirmar
                             </button>
-                            <button type="button" class="min-w-[25%] rounded-lg bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500" onClick={() => setShowAlertDelete(false)}>
+                            <button type="button" className="min-w-[25%] rounded-lg bg-red-400 px-4 py-2 text-sm font-medium text-white hover:bg-red-500" onClick={() => setShowAlertDelete(false)}>
                                 Cancelar
                             </button>
                         </div>
                     </div>
                 </div>
-            </>
             )}
-            {
-                showCorrecto && (
-                    <div className="fixed bottom-0 right-0 mb-8 mr-8">
-                        <div className="flex w-96 shadow-lg rounded-lg">
-                            <div class="bg-green-600 py-4 px-6 rounded-l-lg flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
-                            </div>
-                            <div className="px-4 py-6 bg-white rounded-r-lg flex justify-between items-center w-full border border-l-transparent border-gray-200">
-                                <div>El sistema se limpió correctamente.</div>
-                                <button onClick={() => { setShowCorrecto(!showCorrecto) }}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="fill-current text-gray-700" viewBox="0 0 16 16" width="20" height="20">
-                                        <path fillRule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path>
-                                    </svg>
-                                </button>
-                            </div>
-                        </div>
+            {showCorrecto && (
+                <div className="fixed inset-0 z-10 bg-grey bg-opacity-10 backdrop-blur-sm flex justify-center items-center">
+                    <div className="flex flex-col justify-center content-center items-center rounded-lg bg-white p-4 shadow-2xl min-w-[50vw] max-w-[50vw] border border-solid border-gray-300">
+                        <h2 className="text-lg font-bold">Sistema limpiado correctamente</h2>
                     </div>
-                )
-            }
-        </div >
-    )
+                </div>
+            )}
+        </div>
+    );
 }
 
-export default page
+export default Page;
